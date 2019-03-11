42d5cb05cb78f7fa14c3bbf773a0d59f29e1de79
z84935761 <1287123660@qq.com>
2019-03-11 09:53:15 +0800

    commit the busConfig

---
+++ src/main/java/com/sc/springcloudbusconfig/SpringcloudbusconfigApplication.java
@@ -0,0 +1,13 @@
+package com.sc.springcloudbusconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudbusconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudbusconfigApplication.class, args);
+    }
+
+}
